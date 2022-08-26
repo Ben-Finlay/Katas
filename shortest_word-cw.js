c2fb97d97d31209138976a3df9ba8e71bd5e43b9
Ben Finlay <benjamin.finlay@gmail.com>
2022-08-26 13:49:48 -0400

    Shortest Word

---
+++ shortest_word-cw.js
@@ -0,0 +1,18 @@
+/* Instructions
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+  let holdArr = s.split(' ')
+  let min = Infinity;
+
+  for (let i = 0; i < holdArr.length; i++) {
+    if (holdArr[i].length < min) {
+      min = holdArr[i].length;
+    }
+  
+  }
+  return min
+}
